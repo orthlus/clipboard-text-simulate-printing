47f2117575567d10568e6546ee6d23d62f392502
ort <orthodoxplus@ya.ru>
2024-09-30 08:40:09 +0300

    language

--- src/main/java/art/aelaort/Main.java
+++ src/main/java/art/aelaort/Main.java
@@ -4,12 +4,14 @@ import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.KeyEvent;
+import java.awt.im.InputContext;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 	public static void main(String[] args) {
 		sleep(3);
+		setEnglish();
 		press(getTextFromClipboard());
 	}
 
@@ -50,6 +52,21 @@ public class Main {
 					System.out.printf("error to press '%s' character: %d, %d\n", c,
 							((int) Character.toUpperCase(c)), ((int) c));
 				}
+			}
+		} catch (AWTException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void setEnglish() {
+		try {
+			String currentLanguage = InputContext.getInstance().getLocale().toString();
+			if (!currentLanguage.equals("en_US")) {
+				Robot robot = new Robot();
+				robot.keyPress(KeyEvent.VK_SHIFT);
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				robot.keyRelease(KeyEvent.VK_SHIFT);
 			}
 		} catch (AWTException e) {
 			throw new RuntimeException(e);
